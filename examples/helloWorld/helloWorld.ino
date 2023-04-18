ca4bf2620b5cb311dda7ca19f889bdcba337b33e
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2023-04-18 12:31:54 +0300

    Upd 180423
    Create library, add files

---
+++ examples/helloWorld/helloWorld.ino
@@ -0,0 +1,31 @@
+#include "mg.h"
+
+Systems sys;
+Graphics gfx;
+Interface inf;
+Terminal trm;
+
+void hello()
+{
+    gfx.drawCursor(true,sys.joi0x(),sys.joi0y());
+    gfx.print("text\ntext2", 10, 10, 10, 6);
+}
+
+
+void setup()
+{
+    gfx.screen();
+    inf.greetings();
+    trm.terminal();
+}
+
+void loop()
+{
+    gfx.render(hello,0);
+
+    if (sys.sw0())
+    {
+        Serial.println("hello");
+        inf.messageInfo("game over\ntry again", 1500);
+    }
+}
---
+++ src/makegame.h
@@ -0,0 +1,68 @@
+/*
+    library for MakeGame board.
+
+    [!] Educational version
+    [!] Required u8g2 library
+
+    autor: Savushkin Alexander    
+    git:   @avsavushkiny
+    date:  18.04.2023
+*/
+
+#ifndef MAKEGAME_H
+#define MAKEGAME_H
+
+class Systems
+{
+private:
+public:
+    bool sw0();
+    bool sw1();
+
+    int joi0y();
+    int joi1y();
+    int joi0x();
+    int joi1x();
+
+    void backlight(bool state);
+
+    int obj0y();
+    int obj1y();
+};
+
+class Timer
+{
+private:
+public:
+    void timer(void (*timer_fn)(void), int interval);
+};
+
+class Graphics
+{
+private:
+public:
+    void screen();
+    void render(void (*ptr_draw_fn)(), int timeDelay);
+    void clear();
+    void print(String text, int x, int y, int lii, int chi);
+    bool winkPrint(void (*ptr_fn)(String, int, int), String text, int x, int y, /*delay*/ int interval);
+    bool drawCursor(bool stateCursor, int sysJoi0x, int sysJoi0y);
+    void printf(String text, int x, int y);
+};
+
+class Terminal
+{
+private:
+public:
+    void terminal();
+};
+
+class Interface
+{
+private:
+public:
+    void greetings();
+    void messageInfo(String text, int del);
+};
+
+#endif
